8d8a8e09b0cf4563f955d606c431faa9958e5751
Matheus bastos <105763337+IamNatri@users.noreply.github.com>
2023-03-27 16:53:30 -0300

    IF, IF ELSE, FOR, WHILE, DO WHILE

---
+++ sintaxeVariaveisFluxos/src/sintaxeVariaveisFluxos/TestVariaveis.java
@@ -0,0 +1,19 @@
+package sintaxeVariaveisFluxos;
+
+public class TestVariaveis {
+	public static void main(String[] args) {
+		System.out.println("Novo Teste");
+		
+		int idade;
+		idade = 22;
+		System.out.println(idade);
+		
+		idade+= 10;
+		System.out.println(idade);
+		
+		idade = idade/2 + 11; 
+		System.out.println("A idade é: " + idade);
+	}
+	
+	
+}
